357b2a788cc8f8770436bfb370bb62b2b4a35aac
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-17 16:42:24 +0200

    bonus con jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,27 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+$(document).ready(function () {
+    $('.show-alert-delete-box').click(function (event) {
+        var form = $(this).closest("form");
+        //var name = $(this).data("name");
+        event.preventDefault();
+        swal({
+            title: "Sei sicuro di voler cancellare questo elemento?",
+            text: "Se lo cancelli lo perderai.",
+            icon: "warning",
+            type: "warning",
+            buttons: ["Cancel", "Yes!"],
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((willDelete) => {
+            if (willDelete) {
+                form.submit();
+            }
+        });
+    });
+});
+
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -14,35 +14,11 @@
                             <h6 class="card-text">{{$comic->price}}</h6>
                             <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="btn btn-primary">Info fumetto</a>
                             <a href="{{route('comics.edit', ['comic' => $comic->id])}}" class="btn btn-secondary">Modifica</a>
-                   
-                                <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                                    Launch static backdrop modal
-                                </button>
-                                
-                                <!-- Modal -->
-                                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                        ...
-                                        </div>
-                                        <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <form action="{{route('comics.destroy', ['comic' => $comic->id])}}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Cancella Articolo</button>
-                                        </form>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
-                          
+                            <form method="POST" action="{{route('comics.destroy', ['comic' => $comic->id])}}">
+                                @csrf
+                                <input name="_method" type="hidden" value="DELETE">
+                                <button type="submit" class="btn btn-xs btn-danger btn-flat show-alert-delete-box btn-sm" data-toggle="tooltip" title='Delete'>Delete</button>
+                            </form> 
                         </div>
                     </div>
                 </div>
